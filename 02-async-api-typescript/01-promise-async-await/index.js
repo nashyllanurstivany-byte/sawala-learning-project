0346983118986d125fcf3c810e90fd4526e436da
nashylla <nashyllanurstivany@gmail.com>
2026-09-22 12:49:31 +0700

    add promise

---
+++ 02-async-api-typescript/01-promise-async-await/index.js
@@ -0,0 +1,36 @@
+function pesanMakanan(namaResto, restoBuka) {
+    return new Promise((resolve, reject) => {
+        console.log(`Memesan Makanan dari ${namaResto}...`);
+
+        setTimeout(() => {
+            if (restoBuka) {
+                resolve(`Makanan dari ${namaResto} sudah sampai!`);
+            } else {
+                reject(`Maaf, ${namaResto} sedang tutup.`);
+            }
+        }, 2000);
+    });
+}
+
+async function prosesPesanan() {
+    try {
+        const hasil = await pesanMakanan("Warung Bu Sari", true);
+        console.log(hasil);
+    } catch (error) {
+        console.log("Gagal pesan:", error);
+    }
+
+    console.log("---");
+
+    try {
+        const hasil2 = await pesanMakanan("Resto XYZ", false);
+        console.log(hasil2);
+    } catch (error) {
+        console.log("Gagal pesan:", error);
+    }
+}
+
+prosesPesanan();
+
+
+
